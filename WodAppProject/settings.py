122985df2b63abad4cc56772e925a87fd7fc368c
Bear <bear@homebinder.com>
2015-12-02 10:58:57 +0200

    setting

--- WodAppProject/settings.py
+++ WodAppProject/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/WodApp/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 MEDIA_ROOT = '/WodApp/media/'
 
