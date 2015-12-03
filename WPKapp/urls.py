985733e2306891185e2903540b007c6d3bd5f61f
Bear <bear@homebinder.com>
2015-12-03 11:31:38 +0200

    Static and Template added

--- WPKapp/urls.py
+++ WPKapp/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from registration import views
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'WPKapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', views.signup, name="signup" ),
+    url(r'^login/$', views.login, name="login" ),
+    url(r'^register/success/$', views.register_success, name="register"),
 )
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,84 @@
 from django.shortcuts import render
+from registration.forms import *
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib import messages
 
 # Create your views here.
+@csrf_protect
+def home(request):
+    print("sdfasfdas")
+    return render(request, 'home.html', {})
+
+@csrf_protect
+def signup(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        bRegister = request.POST.get('IsRegister', '')
+        if bRegister == "LogIn":
+            return login(request)
+
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+        else:
+            messages.add_message(request,
+                     messages.ERROR,
+                     "Unable SignUp! "
+                     "Check username/password")
+    else:
+        form = RegistrationForm()
+
+    variables = RequestContext(request, {
+    'form': form
+    })
+
+    return render(request, 'registration/signup.html')
+
+@csrf_protect
+def login(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        bRegister = request.POST.get('IsRegister', '')
+        if bRegister == "Register":
+            return signup(request)
+
+        username = request.POST.get('username', '')
+        password = request.POST.get('password1', '')
+        user = auth.authenticate(username = username, password = password)
+        if user is not None and user.is_active:
+            # Correct password, and the user is marked "active"
+            auth.login(request, user)
+            # Redirect to a success page.
+            print("bbbbbbbbbbbb")
+            return HttpResponseRedirect("/register/loggedin/")
+        else:
+            # Show an error page
+            print("sdfsdfsssss")
+            messages.add_message(request, messages.INFO, "Unable login! " "Check username/password")
+    else:
+        form = LoginForm()
+        print("aaaaaaaaaaaaaa")
+        variables = RequestContext(request, {
+        'form': form
+        })
+
+    return render(request, 'registration/login.html')
+
+@csrf_protect
+def register_success(request):
+    return render(request, 'registration/success.html')
+
+@csrf_protect
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
